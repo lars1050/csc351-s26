b31084db360079846ce20da68057243f30d21a20
Amy C. Larson <larsonam@augsburg.edu>
2026-01-07 09:40:52 -0600

    reformatting comments

--- Sorting-PartOne/MergeIterative.java
+++ Sorting-PartOne/MergeIterative.java
@@ -35,17 +35,21 @@ Creating generic type arrays looks like this:
 T[] arrayCopy = (T[]) Array.newInstance(
 	array.getClass().getComponentType(), n);
 
-Ordering array elements is based on the key. Like this: keyGetter.apply(left[left_idx])).compareTo( 			keyGetter.apply(right[right_idx])) <= 0)
+Ordering array elements is based on the key. 
+Like this: keyGetter.apply(left[left_idx])).compareTo(keyGetter.apply(right[right_idx])) <= 0)
 
 The size of the subarrays to be merged are powers of 2, which are the step size for a pass through the array (except when n is not a power of 2).
+BEWARE arrays that are not powers of 2. Think through an array of size 10 -- how will you handle those extra 2 elements at the end? 
+I recommend first writing code for an array of size 8 (or some 2^x), then fix it to handle any size.
 
-BEWARE arrays that are not powers of 2. Think through an array of size 10 -- how will you handle those extra 2 elements at the end? I recommend first writing code for an array of size 8 (or some 2^x), then fix it to handle any size.
+The number of times you have to pass through the array to merge subarrays is based on log2(n), 
+like this: int passes = (int) (Math.log(n) / Math.log(2));
 
-The number of times you have to pass through the array to merge subarrays is based on log2(n), like this: int passes = (int) (Math.log(n) / Math.log(2));
+It is useful to be explicit about your indices. There are a lot of them in this code. 
+It is probably best to name them (for example, start or left_idx, as opposed to p or i) 
+and double check their value -- super easy to be off by 1.
 
-It is useful to be explicit about your indices. There are a lot of them in this code. It is probably best to name them (for example, start or left_idx, as opposed to p or i) and double check their value -- super easy to be off by 1.
-
-There are elements in the iterative version that also appear in the recursive version (which has been completed for you). Feel free to copy those elements into this algorithm.
-
+There are elements in the iterative version that also appear in the recursive version (which has been completed for you). 
+Feel free to copy those elements into this algorithm.
 */
 
